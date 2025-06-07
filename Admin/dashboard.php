9ba1ed8c816375c72b0b8adec8dffdef4baf8d5e
Stephen kinyua <stevo4309@gmail.com>
2025-06-07 21:30:27 +0300

    Update admin_template and dashboard files

--- Admin/dashboard.php
+++ Admin/dashboard.php
@@ -1,11 +1,3 @@
-<?php
-session_start();
-if (!isset($_SESSION['admin_logged_in']) || $_SESSION['admin_logged_in'] !== true) {
-    header("Location: admin_login.php");
-    exit;
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
