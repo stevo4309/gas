1a9c999f6db4543bbefc45cd1353fd1960d3d649
Stephen kinyua <stevo4309@gmail.com>
2025-06-08 11:06:30 +0300

    Fix redirect to Admin_login.php

--- Admin/index.php
+++ Admin/index.php
@@ -1,7 +1,7 @@
 <?php  
 session_start();
 if (!isset($_SESSION['admin_logged_in'])) {
-    header('Location: login.php');
+header("Location: Admin_login.php");
     exit();
     $page = 'dashboard.php'; 
 include 'admin_template.php';
