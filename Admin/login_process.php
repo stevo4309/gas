3c20c3a34792a801045968af22ca247cf72d370e
Stephen kinyua <stevo4309@gmail.com>
2025-06-08 11:18:19 +0300

    Update login process logic

--- Admin/login_process.php
+++ Admin/login_process.php
@@ -2,8 +2,8 @@
 session_start();
 
 // Example hardcoded credentials (replace with DB check)
-$admin_username = 'admin';
-$admin_password = '123456';
+$admin_username = 'joysmartgas';
+$admin_password = '42009526';
 
 if ($_POST['username'] === $admin_username && $_POST['password'] === $admin_password) {
     $_SESSION['admin_logged_in'] = true;
